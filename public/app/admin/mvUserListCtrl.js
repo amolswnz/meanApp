9065244aa18361b846a030e361f929b180fec172
Amol Wankhede <amolsw.nz@gmail.com>
2017-02-27 09:26:00 +1300

    Securing routes on client side

---
+++ public/app/admin/mvUserListCtrl.js
@@ -0,0 +1,8 @@
+(function() {
+    'use strict';
+
+    angular.module('app')
+        .controller('mvUserListCtrl', function($scope, mvUser) {
+            $scope.users = mvUser.query();
+        });
+}());
--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,34 @@
 (function() {
     angular.module('app', ['ngResource', 'ngRoute'])
         .config(function($routeProvider, $locationProvider) {
+            $locationProvider.html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+
             $routeProvider
                 .when('/', {
                     templateUrl: '/partials/main/main',
                     controller: 'mvMainCtrl'
+                })
+                .when('/admin/users', {
+                    templateUrl: '/partials/admin/user-list',
+                    controller: 'mvUserListCtrl',
+                    resolve: {
+                        auth: function(mvIdentity, $q) {
+                            if(mvIdentity.currentUser && mvIdentity.currentUser.roles.indexOf('Admin') > -1)
+                                return true;
+                            else
+                                return $q.reject('na');
+                        }
+                    }
                 });
         });
+
+    angular.module('app').run(function($rootScope, $location) {
+        $rootScope.$on('$routeChangeError', function(evt, current, prev, rejection) {
+            if(rejection === 'na')
+                $location.path('/');
+        });
+    });
 }());
