5bcc238e8c3e12aa17f90941b6e02e8d94e778d0
Amol Wankhede <amolsw.nz@gmail.com>
2017-02-26 16:19:47 +1300

    Logout code

--- public/app/account/mvAuth.js
+++ public/app/account/mvAuth.js
@@ -16,6 +16,14 @@
                             }
                         });
                     return dfd.promise;
+                },
+                logoutUser: function() {
+                    var dfd = $q.defer();
+                    $http.post('/logout', { logout:true }).then(function() {
+                        mvIdentity.currentUser = undefined;
+                        dfd.resolve();
+                    });
+                    return dfd.promise;
                 }
             };
         });
--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
     angular.module('app')
-        .controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth) {
+        .controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth, $location) {
             $scope.identity = mvIdentity;
             $scope.signin = function(username, password) {
                 console.log('login cliked');
@@ -13,6 +13,14 @@
                             mvNotifier.notify('ERROR logging in. Incorrect username/password!');
                         }
                     });
+             };
+             $scope.signout = function() {
+                 mvAuth.logoutUser().then(function() {
+                     $scope.username = "";
+                     $scope.password = "";
+                     mvNotifier.notify('You have been successfully logged out !');
+                     $location.path('/');
+                 });
              };
         });
 }());
